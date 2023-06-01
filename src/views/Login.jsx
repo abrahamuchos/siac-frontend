5a086352ba399307dccaf1349145e5080759f041
abrahamuchos <agonzalez@wayuinc.com>
2023-06-01 09:30:18 -0400

    Add styles to login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { useUserContext } from "../contexts/UserProvider.jsx";
 import { Navigate, useNavigate } from "react-router-dom";
 import axiosClient from "../axios-client.js";
+import { Button, Card, FloatingLabel, Form } from "react-bootstrap";
+import '../scss/pages/login.scss';
+import Footer from "../components/Footer.jsx";
 
 export default function Login() {
   const {token, setToken, setUser, user} = useUserContext();
@@ -29,18 +32,39 @@ export default function Login() {
   }
 
   return (
-    <div>
-      <h1>Login!!</h1>
-      <button onClick={(e) => handleLogin(e)}>Login</button>
-      { isLoading ? null :
-        (user.role?.id === 1  && token? <Navigate to='/admin' replace/> :
+    <>
+      <Card id='login'>
+        <Card.Body>
+          <div className='card-img text-center'>
+            <img src="https://placehold.co/800x400?text=Logo" alt="UPCM Logo"/>
+          </div>
+          <Form className='d-flex flex-column justify-content-center m-auto'>
+            <FloatingLabel label='Correo electrónico' controlId='email'>
+              <Form.Control type='email' placeholder='nombre@correo.com'/>
+            </FloatingLabel>
+            <FloatingLabel label='Contraseña' controlId='password'>
+              <Form.Control type='password' placeholder='contraseña'/>
+              <small className='float-end'>
+                <a href="/#" className='text-decoration-none'>Olvide la contraseña</a>
+              </small>
+            </FloatingLabel>
+            <div className='d-grid gap-2 d-md-block text-center mt-3'>
+              <Button variant='primary'>Iniciar sesión</Button>
+            </div>
+
+          </Form>
+        </Card.Body>
+      </Card>
+      <Footer/>
+
+      {isLoading ? null :
+        (user.role?.id === 1 && token ? <Navigate to='/admin' replace/> :
             user.role?.id === 2 && token ? <Navigate to='/um' replace/> :
               user.role?.id === 3 && token ? <Navigate to='/doctor' replace/> :
                 user.role?.id === 4 && token ? <Navigate to='/assistant' replace/> :
                   <Navigate to='/404' replace/>
         )
-
       }
-    </div>
+    </>
   );
 }
