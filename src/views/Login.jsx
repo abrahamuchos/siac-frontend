80077da573d2e1671bf17fe6efb08c7d73d63509
abrahamuchos <agonzalez@wayuinc.com>
2023-06-01 16:56:51 -0400

    Add login component

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,22 +1,31 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useUserContext } from "../contexts/UserProvider.jsx";
-import { Navigate, useNavigate } from "react-router-dom";
 import axiosClient from "../axios-client.js";
-import { Button, Card, FloatingLabel, Form } from "react-bootstrap";
-import '../scss/pages/login.scss';
+import { Navigate } from "react-router-dom";
+import { Alert, Button, Card, FloatingLabel, Form } from "react-bootstrap";
+import { Formik, useFormik } from "formik";
+import { object, string } from "yup";
 import Footer from "../components/Footer.jsx";
+
+import '../scss/pages/login.scss';
 
 export default function Login() {
   const {token, setToken, setUser, user} = useUserContext();
   const [isLoading, setIsLoading] = useState(true);
-  // const navigate = useNavigate();
+  const [errors, setErrors] = useState(false);
 
-  const handleLogin = (e) => {
-    e.preventDefault();
+  let loginSchema = object({
+    email: string().email('Email invalido, ej. hello@correo.com').required('Correo electrónico es un campo requerido'),
+    password: string().min(3).required('Contraseña es un campo requerido')
+  });
+
+  const handleLogin = (values, setSubmitting) => {
+    setErrors(false)
     setIsLoading(true)
+    setSubmitting(true);
     const payload = {
-      email: 'bridgette46@gmail.com',
-      password: 'password'
+      email: values.email,
+      password: values.password
     }
 
     axiosClient.post('/login', payload)
@@ -26,45 +35,69 @@ export default function Login() {
         setIsLoading(false)
       })
       .catch((err) => {
+        let response = err.response;
         console.error(err)
         setIsLoading(false)
+        setSubmitting(false);
+        if(response && response.status === 422){
+          setErrors('Correo electrónico o contraseña incorrecta, intente nuevamente');
+        }
       });
   }
 
-  return (
-    <>
-      <Card id='login'>
-        <Card.Body>
-          <div className='card-img text-center'>
-            <img src="https://placehold.co/800x400?text=Logo" alt="UPCM Logo"/>
-          </div>
-          <Form className='d-flex flex-column justify-content-center m-auto'>
-            <FloatingLabel label='Correo electrónico' controlId='email'>
-              <Form.Control type='email' placeholder='nombre@correo.com'/>
-            </FloatingLabel>
-            <FloatingLabel label='Contraseña' controlId='password'>
-              <Form.Control type='password' placeholder='contraseña'/>
-              <small className='float-end'>
-                <a href="/#" className='text-decoration-none'>Olvide la contraseña</a>
-              </small>
-            </FloatingLabel>
-            <div className='d-grid gap-2 d-md-block text-center mt-3'>
-              <Button variant='primary'>Iniciar sesión</Button>
-            </div>
 
-          </Form>
-        </Card.Body>
-      </Card>
-      <Footer/>
+  return (<>
+    <Card id='login'>
+      <Card.Body>
+        <div className='card-img text-center'>
+          <img src="https://placehold.co/800x400?text=Logo" alt="UPCM Logo"/>
+        </div>
 
-      {isLoading ? null :
-        (user.role?.id === 1 && token ? <Navigate to='/admin' replace/> :
-            user.role?.id === 2 && token ? <Navigate to='/um' replace/> :
-              user.role?.id === 3 && token ? <Navigate to='/doctor' replace/> :
-                user.role?.id === 4 && token ? <Navigate to='/assistant' replace/> :
-                  <Navigate to='/404' replace/>
-        )
-      }
-    </>
-  );
+        <Formik initialValues={{email: '', password: ''}} validationSchema={loginSchema}
+                onSubmit={(values, {setSubmitting}) => handleLogin(values, setSubmitting)}>
+          {({handleSubmit, values, handleChange, touched, errors, isSubmitting}) => (
+            <Form className='d-flex flex-column justify-content-center m-auto' noValidate onSubmit={handleSubmit}>
+              <FloatingLabel label='Correo electrónico' controlId='email'>
+                <Form.Control type='email' placeholder='nombre@correo.com' name='email' value={values.email}
+                              onChange={handleChange} className={touched.email && errors.email ? "error-input" : null}/>
+                {touched.email && errors.email ? (
+                  <div className="error-input-message">{errors.email}</div>
+                ) : null}
+              </FloatingLabel>
+              <FloatingLabel label='Contraseña' controlId='password'>
+                <Form.Control type='password' placeholder='contraseña' name='password' value={values.password}
+                              onChange={handleChange}
+                              className={touched.password && errors.password ? "error-input" : null}/>
+                {touched.password && errors.password ? (
+                  <div className="error-input-message">{errors.password}</div>
+                ) : null}
+                <small className='float-end mt-2'>
+                  <a href="/#" className='text-decoration-none'>Olvide la contraseña</a>
+                </small>
+              </FloatingLabel>
+
+              <div className='d-grid gap-2 d-md-block text-center mt-3'>
+                <Button variant='primary' type='submit' disabled={isSubmitting}>Iniciar sesión</Button>
+              </div>
+            </Form>
+          )}
+
+        </Formik>
+
+      </Card.Body>
+    </Card>
+    <Footer/>
+
+    { errors ?
+      <Alert variant='danger' dismissible>
+        <div>{errors}</div>
+      </Alert> : ''
+    }
+
+    {isLoading ? null : (user.role?.id === 1 && token ?
+      <Navigate to='/admin' replace/> : user.role?.id === 2 && token ?
+        <Navigate to='/um' replace/> : user.role?.id === 3 && token ?
+          <Navigate to='/doctor' replace/> : user.role?.id === 4 && token ? <Navigate to='/assistant' replace/> :
+            '')}
+  </>);
 }
